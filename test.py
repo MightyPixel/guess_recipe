b4d6449c07463da72ead43f1cdbdc2cc15268363
Ognyan Angelov <sick.java.junkie@gmail.com>
2016-02-12 22:42:06 +0200

    Modify statistic plot.

--- test.py
+++ test.py
@@ -2,13 +2,14 @@ import pylab
 
 from main import *
 
-PROBS = range(1, 9)
-REPEAT_COUNT = 10
+PROBS = range(5, 9)
+REPEAT_COUNT = 3
 
 pylab.figure(1)
 pylab.title('Crossover Prob/Avg population')
 pylab.xlabel('Crossover Prob')
 pylab.ylabel('Avg Generations')
+pylab.grid(True)
 
 lasagna = ["tomatoes", "steam", "cheese", "melt", "sweet Italian sausage", "mix", "garlic"]
 
@@ -17,10 +18,11 @@ for i in PROBS:
     results = []
     for i in range(REPEAT_COUNT):
         result = GeneticAlgorithm(GuessRecipe(lasagna, prob_crossover = 0.1 * i)).run()
-        results.append(len(result))
+        results.append(result)
 
     values.append(sum(results) / len(results))
 
+print values
 pylab.plot(values, PROBS)
-# pylab.savefig('crossoverprob')
+pylab.savefig('crossoverprob')
 pylab.show()
